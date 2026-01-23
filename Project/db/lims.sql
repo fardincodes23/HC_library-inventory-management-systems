625fd1b434a6a6f6263b9fdf7cef15dded2b441d
fardinRafat-admin <frafat@hollandcollege.com>
2026-01-22 21:25:02 -0400

    PROJECT: Added users table into database

--- Project/db/lims.sql
+++ Project/db/lims.sql
@@ -11,3 +11,18 @@ CREATE TABLE IF NOT EXISTS books (
 INSERT INTO books (id,title, type, publisher) VALUES
 ('1','Clean Code', 'Programming', 'Prentice Hall'),
 ('2', 'The Pragmatic Programmer', 'Programming', 'Addison-Wesley');
+
+
+
+
+USE lims;
+
+CREATE TABLE IF NOT EXISTS users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(100) NOT NULL UNIQUE,
+    password_hash VARCHAR(255) NOT NULL,
+    role ENUM('ADMIN','USER') NOT NULL
+    );
+
+INSERT INTO users (username, password_hash, role)
+VALUES ('admin', SHA2('admin123', 256), 'ADMIN');
